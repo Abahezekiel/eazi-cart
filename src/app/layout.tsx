1d8b1e8fc3a6ff32088c3fd6959c878fc03f6d04
Eazy <abahezekiel75@gmail.com>
2025-06-03 23:15:12 +0100

    changes to top nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { Metadata } from "next";
 import "./globals.css";
-import { useState } from "react";
-import { CiSearch } from "react-icons/ci";
+import Image from "next/image";
+import screenshot from "./Images/Screenshot 2025-05-31 at 11.45.46 AM.png"; 
+import { FaRegUser, FaSearch } from "react-icons/fa";
+import { HiOutlineShoppingBag } from "react-icons/hi";
 
 export default function RootLayout({
   children,
@@ -10,22 +12,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col h-screen">
-        <header className="bg-blue-200 p-3">
-         <div>
-         <ul className="flex gap-5">
-            <li>HOME</li>
-            <li>COLLECTION</li>
-            <li>ABOUT</li>
-            <li>CONTACT</li>
-          </ul>
-         </div>
-
-         <div>
-         <CiSearch />
-
-         </div>
-        </header>
+      <body className="flex flex-col h-screen ml-32 mr-32  ">
+        <div className="bg-amber-400">
+          <div className="flex justify-between items-center">
+            <div>
+              <Image
+                src={screenshot}
+                alt="Screenshot"
+                width={100}
+                height={100}
+              />
+            </div>
+            <ul className="flex gap-5 items-center justify-center">
+              <li>HOME</li>
+              <li>COLLECTION</li>
+              <li>ABOUT</li>
+              <li>CONTACT</li>
+              <li>
+                {" "}
+                <button className="border border-gray-400 rounded-2xl p-1 w-28 text-sm font-semibold">
+                  Admin panel
+                </button>
+              </li>
+            </ul>
+            <div className="flex items-center gap-4">
+              <FaSearch />
+              <FaRegUser />
+              <HiOutlineShoppingBag />
+            </div>
+          </div>
+        </div>
 
         <main className="flex-grow">{children}</main>
 
