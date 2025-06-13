9ad71656dce2a19b96fd2327b84087fdeba5cdad
Eazy <abahezekiel75@gmail.com>
2025-06-13 11:20:51 +0100

    admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1146,8 +1146,8 @@ export default function TopBar() {
         />
       </div> */}
 
-      {/* admin dashboard  */}
-      <div className="h-screen bg-[#F9FAFB] flex items-center justify-center ">
+      {/* admin dashboard login  */}
+      {/* <div className="h-screen bg-[#F9FAFB] flex items-center justify-center ">
         <div className="bg-white shadow-md w-80 p-5 rounded-lg">
           <p className="text-lg font-bold mb-2">Admin Panel</p>
           <p className="mb-1 mt-3">Email Address</p>
@@ -1165,6 +1165,30 @@ export default function TopBar() {
           <button className="bg-black text-white w-full p-2 rounded mt-3">
             Login
           </button>
+        </div>
+      </div> */}
+
+      {/* admin dashboard page  */}
+      <div className="h-screen bg-[#F9FAFB]  ">
+        <div className="flex justify-between w-full">
+          <img
+            src="http://localhost:3000/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FScreenshot%202025-05-31%20at%2011.45.46%20AM.a209a70e.png&w=256&q=75"
+            className="w-30"
+          />
+          <button className="bg-gray-500 rounded-3xl text-white m-1 w-28 ">
+            Logout
+          </button>
+        </div>
+        <p className="border-[#E5E7EB] border-t mt-2"></p>
+        <div className="flex w-full bg-[#F9FAFB] h-96">
+          <div className="w-[20%] border-[#E5E7EB] border-r-2">
+            <div>
+              <button className="bg-[#FFEBF5] border-[#E5E7EB] border-r-2">
+                Add Items
+              </button>
+            </div>
+          </div>
+          <div className="w-[80%]"></div>
         </div>
       </div>
     </>
