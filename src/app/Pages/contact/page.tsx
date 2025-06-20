fb01f94d8382d7efc2fe1b4aa741079c86fc6ef1
Eazy <abahezekiel75@gmail.com>
2025-06-20 22:29:02 +0100

    change

---
+++ src/app/Pages/contact/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "contact",
+  },
+  };
+
+
+export default function Contact(){
+  return <h1>contact page</h1>;
+}
+
+
+
